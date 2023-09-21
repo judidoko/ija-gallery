44ad80954bf753a9fe0f83d0312392130ac8e6ed
Jude Idoko <judidoko@gmail.com>
2023-09-21 18:59:18 +0100

    correcting drag

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -2,12 +2,19 @@ import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
 const ImageCard = ({ item }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: item.id });
+  const {
+    isDragging,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: item.id });
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
+    opacity: isDragging ? 0.5 : 1,
   };
   return (
     <div
